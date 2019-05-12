9c07787e8595da5e6085254720efc801642fb6c0
krom121 <stephenacgregor9@gmail.com>
2019-05-12 16:48:12 +0100

    commented out staic data frontend

--- src/app/car.service.ts
+++ src/app/car.service.ts
@@ -11,37 +11,37 @@ export class CarService {
   order: Order;
 
   constructor() {
-    this.model = {
-      base_price: 30000,
-      engines: [
-        {id: 1, name: "Uno", image: "/assets/engine-1.png", price: 500},
-        {id: 2, name: "Duo", image: "/assets/engine-2.png", price: 1000},
-        {id: 3, name: "Tree", image: "/assets/engine-3.png", price: 2000},
-      ],
-      wheels: [
-        {id: 1, name: "18", image: "/assets/wheels-18.png", price: 300},
-        {id: 2, name: "19", image: "/assets/wheels-19.png", price: 500},
-      ],
-      interiors: [
-        {id: 1, name: "Black", image: "/assets/interior-black.png", price: 0},
-        {id: 2, name: "Brown", image: "/assets/interior-brown.png", price: 500},
-      ],
-    };
+    //this.model = {
+      //base_price: 30000,
+     // engines: [
+        //{id: 1, name: "Uno", image: "/assets/engine-1.png", price: 500},
+        //{id: 2, name: "Duo", image: "/assets/engine-2.png", price: 1000},
+        //{id: 3, name: "Tree", image: "/assets/engine-3.png", price: 2000},
+      //],
+      //wheels: [
+       // {id: 1, name: "18", image: "/assets/wheels-18.png", price: 300},
+       // {id: 2, name: "19", image: "/assets/wheels-19.png", price: 500},
+      //],
+      //interiors: [
+        //{id: 1, name: "Black", image: "/assets/interior-black.png", price: 0},
+        //{id: 2, name: "Brown", image: "/assets/interior-brown.png", price: 500},
+      //],
+    //};
 
-    this.order = {
-      engine: this.model.engines[0],
-      wheels:this.model.wheels[0],
-      exterior_color: "#ffffff",
-      interior: this.model.interiors[0],
-      autopilot: false,
+    //this.order = {
+      //engine: this.model.engines[0],
+      //wheels:this.model.wheels[0],
+      //exterior_color: "#ffffff",
+      //interior: this.model.interiors[0],
+      //autopilot: false,
 
-      firstname: '',
-      lastname: '',
-      phone: '',
-      email: '',
-      card_number: '',
-      card_date: '',
-    }
+      //firstname: '',
+      //lastname: '',
+      //phone: '',
+     // email: '',
+      //card_number: '',
+      //card_date: '',
+    //}
 
 
    }
